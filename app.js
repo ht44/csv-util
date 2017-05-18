0d9ef430dd3b5e460cfc226b2355eb362b7ebaa2
Hayden Turek <haydenturek@gmail.com>
2017-05-18 15:07:47 -0500

    filter out ones we have

--- app.js
+++ app.js
@@ -1,10 +1,48 @@
 //'use strict';
 const fs = require('fs');
 
-fs.readdir('./csv_files', 'utf8', (err, files) => {
-    let output = [];
+const austin24 = [
+    "Ailanthus_altissima",
+    "Lonicera_japonica",
+    "Arundo_donax",
+    "Macfadyena_unguis_cati",
+    "Bothriochloa_ischaemum_var_songarica",
+    "Melia_azedarach",
+    "Broussonetia_papyrifera",
+    "Nandina_domestica",
+    "Centaurea_melitensis",
+    "Phyllostachys_aurea",
+    "Colocasia_esculenta",
+    "Pistacia_chinensis",
+    "Cynodon_dactylon",
+    "Pueraria_montana",
+    "Cyrtomium_falcatum",
+    "Pyracantha_coccinea",
+    "Eichhornia_crassipes",
+    "Rapistrum_rugosum",
+    "Firmiana_simplex",
+    "Sorghum_halepense",
+    "Hydrilla_verticillata",
+    "Tamarex_ramosissima",
+    "Ligustrum_lucidum",
+    "Triadica_sebifera",
+    "Albizia_julibrissin",
+    "Ligustrum_quihoui",
+    "Ligustrum_sinense"
+];
 
+console.log(austin24.length);
+
+fs.readdir('./csv_files/', 'utf8', (err, files) => {
+    let output = [];
+    let trimmedFiles = [];
     files.forEach(filename => {
+      if (!austin24.includes(filename)) {
+        trimmedFiles.push(filename);
+      }
+    });
+
+    trimmedFiles.forEach(filename => {
         let contents = fs.readFileSync(`./csv_files/${filename}`, 'utf8');
         let ids = [];
         let result = {
@@ -26,11 +64,11 @@ fs.readdir('./csv_files', 'utf8', (err, files) => {
         output.push(result);
 
     });
-
+    console.log(output);
     output = JSON.stringify(output);
     fs.writeFile('./output.js', output, (err) => {
         if (err)
             console.error(err);
-        console.log(JSON.parse(output));
+        // console.log(JSON.parse(output));
     });
 });
